fe485114ded9714512bb2fce5fe753d80c25913e
ruibamboo6-oss <ruibamboo6@gmail.com>
2026-09-18 11:03:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function sendInquiry() {
-    const name = document.getElementById("name").value;
 
-    alert("Hello " + name + "! We received your inquiry.");
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const product = document.getElementById("product").value;
+    const message = document.getElementById("message").value;
+
+    alert(
+        "Company: " + name +
+        "\nEmail: " + email +
+        "\nProduct: " + product +
+        "\nMessage: " + message
+    );
 }
